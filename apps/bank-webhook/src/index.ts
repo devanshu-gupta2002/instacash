b0bb3a4d0fc26ec78343f83ebfb918d06a26254c
devanshug2910 <devanshu@useteleport.com>
2024-07-04 01:29:32 +0530

    feat: shema update, onramp functionality

--- apps/bank-webhook/src/index.ts
+++ apps/bank-webhook/src/index.ts
@@ -1,11 +1,48 @@
 import  express  from "express";
-
+import database from "@repo/db/client"
 const app = express()
 
-app.post('/hdfcWebhook', (req, res) => {
-  const paymentInformation = {
+app.post('/hdfcWebhook', async (req, res) => {
+  const db = database.default
+  const paymentInformation : {
+    token: string
+    userId: string
+    amount: string
+  } = {
     token: req.body.token,
     userId: req.body.user_identifier,
     amount: req.body.amount
   }
+
+ try {
+  await db.$transaction([
+    db.balance.updateMany({
+      where: {
+        userId: Number(paymentInformation.userId)
+      },
+      data: {
+        amount: {
+          increment: Number(paymentInformation.amount)
+        }
+      }
+    }),
+    db.onRampTransaction.updateMany({
+      where: {
+        token: paymentInformation.token
+      },
+      data: {
+        status: "Success"
+      }
+    })
+  ])
+
+  res.json({
+    message: "Captured"
+  })
+} catch (error) {
+    console.log(error)
+    res.status(411).json({
+      message: "Error while processing webhook"
+    })
+ } 
 })
